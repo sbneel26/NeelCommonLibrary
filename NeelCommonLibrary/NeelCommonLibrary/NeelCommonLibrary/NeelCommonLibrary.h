3ce366b2033e313e0d46d1cc4d46d2b96e418c08
sbhardwaj-persistent <sunil_bhardwaj@persistent.com>
2019-04-27 22:45:50 +0530

    license initial commit

---
+++ NeelCommonLibrary/NeelCommonLibrary/NeelCommonLibrary/NeelCommonLibrary.h
@@ -0,0 +1,19 @@
+//
+//  NeelCommonLibrary.h
+//  NeelCommonLibrary
+//
+//  Created by Sunil Bhardwaj on 4/27/19.
+//  Copyright © 2019 Sunshine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeelCommonLibrary.
+FOUNDATION_EXPORT double NeelCommonLibraryVersionNumber;
+
+//! Project version string for NeelCommonLibrary.
+FOUNDATION_EXPORT const unsigned char NeelCommonLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeelCommonLibrary/PublicHeader.h>
+
+
---
+++ NeelCommonLibrary/README.md
@@ -0,0 +1,1 @@
+# This is the common utility library which we can use in our project without creating again and again in different projects. We can just add this library in our project and boom, we are ready to use the most common helpful methods in our projects.
